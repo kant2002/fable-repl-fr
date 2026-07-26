ce471cfd09b09fff3c0e9460a0bf165684746bb8
Andrii Kurdiumov <kant2002@gmail.com>
2026-07-26 10:44:28 +0200

    Traduire units.fs

--- samples/tour/units.fs
+++ samples/tour/units.fs
@@ -7,25 +7,25 @@ module Visite.UnitésDeMesure
 // Les unités de mesure sont un moyen d'annoter les types numériques primitifs de manière sécurisée.
 // Vous pouvez ensuite effectuer des opérations arithmétiques sur ces valeurs de manière sécurisée.
 //
-// Pour en savoir plus, consultez : https://docs.microsoft.com/dotnet/fsharp/language-reference/units-of-measure
+// Pour en savoir plus, consultez : https://docs.microsoft.com/dotnet/fsharp/language-reference/units-de-measure
 
 // Tout d'abord, ouvrez une liste de noms de mesures courants
-open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
+ouvrir Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
 
 /// Définissez une constante unifiée
-let valeurD'exemple1 = 1600.0<metre>
+laisser valeurD'exemple1 = 1600.0<metre>
 
-/// Ensuite, définissez un nouveau type d'unité
+/// Ensuite, définissez un nouveau taper d'unité
 [<Measure>]
-type mille =
+taper mille =
     /// Facteur de conversion miles en mètres
-    static member enMetrès = 1609.34<metre/mille>
+    statique membre enMetrès = 1609.34<metre/mille>
 
 /// Définir une constante unifiée
-let valeurD'exemple2 = 500.0<mille>
+laisser valeurD'exemple2 = 500.0<mille>
 
 /// Calculer la constante du système métrique
-let valeurD'exemple3 = valeurD'exemple2 * mille.enMetrès
+laisser valeurD'exemple3 = valeurD'exemple2 * mille.enMetrès
 
-// Les valeurs utilisant les unités de mesure peuvent être utilisées comme le type numérique primitif pour des choses comme l'impression.
+// Les valeurs utilisant les unités de mesure peuvent être utilisées comme le taper numérique primitif pour des choses comme l'impression.
 printfn "Après une course de %f, je marcherais %f miles, ce qui équivaudrait à %f mètres" valeurD'exemple1 valeurD'exemple2 valeurD'exemple3
