6dc2fe0f0b7344a8cf1c800e52101e35b4018d5d
Andrii Kurdiumov <kant2002@gmail.com>
2026-07-26 11:32:10 +0200

    Traduire records.fs

--- samples/tour/records.fs
+++ samples/tour/records.fs
@@ -7,72 +7,72 @@ module Visite.TuplesEtEnregistrements
 module Tuples =
 
     /// Un tuple simple d'entiers.
-    let tuple1 = (1, 2, 3)
+    laisser tuple1 = (1, 2, 3)
 
     /// Une fonction qui inverse l'ordre de deux valeurs dans un tuple.
     ///
-    /// La déduction de type F# généralisera automatiquement la fonction pour qu'elle ait un type générique,
-    /// ce qui signifie qu'elle fonctionnera avec n'importe quel type.
-    let échangerLesÉléments (a, b) = (b, a)
+    /// La déduction de taper F# généralisera automatiquement la fonction pour qu'elle ait un taper générique,
+    /// ce qui signifie qu'elle fonctionnera avec n'importe quel taper.
+    laisser échangerLesÉléments (a, b) = (b, a)
 
     printfn "Le résultat de l'échange (1, 2) est %A" (échangerLesÉléments (1,2))
 
     /// Un tuple comprenant un entier, une chaîne de caractères
     /// et un nombre à virgule flottante de précision double.
-    let tuple2 = (1, "fred", 3.1415)
+    laisser tuple2 = (1, "fred", 3.1415)
 
     printfn "tuple1: %A\ttuple2: %A" tuple1 tuple2
 
 
 module TypesEnregistrements =
 
-    /// Cet exemple montre comment définir un nouveau type d'enregistrement.
-    type CarteDeContact =
+    /// Cet exemple montre comment définir un nouveau taper d'enregistrement.
+    taper CarteDeContact =
         { Nom     : string
           Phone    : string
           Vérifié : bool }
 
-    /// Cet exemple montre comment instancier un type d'enregistrement.
-    let contact1 =
+    /// Cet exemple montre comment instancier un taper d'enregistrement.
+    laisser contact1 =
         { Nom = "Alf"
           Phone = "(206) 555-0157"
-          Vérifié = false }
+          Vérifié = faux }
 
     /// Vous pouvez également le faire en une seule ligne avec des séparateurs ';'.
-    let contactSurLaMêmeLigne = { Nom = "Alf"; Phone = "(206) 555-0157"; Vérifié = false }
+    laisser contactSurLaMêmeLigne = { Nom = "Alf"; Phone = "(206) 555-0157"; Vérifié = faux }
 
     /// Cet exemple montre comment utiliser l'"opération copie et mise à jour" sur les valeurs d'enregistrement. Il crée une nouvelle valeur
     /// d'enregistrement qui est une copie de contact1, mais avec des valeurs différentes pour les champs "Téléphone" et "Vérifié".
     ///
-    /// Pour en savoir plus, consultez : https://docs.microsoft.com/dotnet/fsharp/language-reference/copy-and-update-record-expressions
-    let contact2 =
-        { contact1 with
+    /// Pour en savoir plus, consultez : https://docs.microsoft.com/dotnet/fsharp/language-reference/copy-et-update-record-expressions
+    laisser contact2 =
+        { contact1 avec
             Phone = "(206) 555-0112"
-            Vérifié = true }
+            Vérifié = vraie }
 
     /// Cet exemple montre comment écrire une fonction qui traite une valeur d'enregistrement.
     /// Il convertit un objet "ContactCard" en chaîne de caractères.
-    let afficherLaCarteDeContact (c: CarteDeContact) =
-        c.Nom + " Phone: " + c.Phone + (if not c.Vérifié then " (non vérifié)" else "")
+    laisser afficherLaCarteDeContact (c: CarteDeContact) =
+        c.Nom + " Phone: " + c.Phone + (si not c.Vérifié alors " (non vérifié)" autre "")
 
     printfn "Carte de contact d'Alf: %s" (afficherLaCarteDeContact contact1)
 
     /// Voici un exemple d'un Enregistrement avec un membre.
-    type CarteDeContactAlternate =
+    taper CarteDeContactAlternate =
         { Nom     : string
           Phone    : string
           Adresse  : string
           Vérifié : bool }
 
         /// Les membres peuvent implémenter des membres orientés objet.
-        member this.CarteDeContactImprimeé =
-            this.Nom + " Phone: " + this.Phone + (if not this.Vérifié then " (non vérifié)" else "") + this.Adresse
+        membre this.CarteDeContactImprimeé =
+            this.Nom + " Phone: " + this.Phone + (si not this.Vérifié alors " (non vérifié)" autre "") + this.Adresse
 
-    let contactAlternate =
+    laisser contactAlternate =
         { Nom = "Alf"
           Phone = "(206) 555-0157"
-          Vérifié = false
+          Vérifié = faux
           Adresse = "111 Alf Street" }
 
-    // Les membres sont accessibles via l'opérateur "." sur un type instancié.
+    // Les membres sont accessibles via l'opérateur "." sur un taper instancié.
     printfn "La carte de contact alternative d'Alf est %s" contactAlternate.CarteDeContactImprimeé
